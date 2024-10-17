e8e3935a91a4624d8a42ea35c0c1638f553cfd46
Gustavo L. Carvalho <gustavocrvlh.02@gmail.com>
2024-10-17 18:36:40 -0300

    add get and set methods

--- Models/Music.cs
+++ Models/Music.cs
@@ -1,25 +1,39 @@
 class Music
 {
-    public string name;
-    public string artist;
-    public bool isSingle;
-    public string? album;
-    public int year;
+    public string Name { get; set; }
+    public string Artist { get; set; }
+    public bool IsSingle { get; set; }
+    public string Album { get; set; }
+    public int Year { get; set; }
+    public string Description
+    {
+        get
+        {
+            if (IsSingle)
+            {
+                return $"This is a single named {Name} by {Artist}, release in {Year}.";
+            }
+            else
+            {
+                return $"This is a song named {Name} from the album {Album} by {Artist}, released in {Year}.";
+            }
+        }
+    }
 
     public void DisplayMusicInfo()
     {
-        if (isSingle)
+        if (IsSingle)
         {
-            Console.WriteLine($"Name: {name} - Single");
-            Console.WriteLine($"Artist: {artist}");
-            Console.WriteLine($"Year: {year}");
+            Console.WriteLine($"Name: {Name} - Single");
+            Console.WriteLine($"Artist: {Artist}");
+            Console.WriteLine($"Year: {Year}");
         }
         else
         {
-            Console.WriteLine($"Name: {name}");
-            Console.WriteLine($"Artist: {artist}");
-            Console.WriteLine($"Album: {album}");
-            Console.WriteLine($"Year: {year}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Artist: {Artist}");
+            Console.WriteLine($"Album: {Album}");
+            Console.WriteLine($"Year: {Year}");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,18 +1,23 @@
-﻿Music music1 = new Music();
+﻿
 
-music1.name = "Dead Embryonic Cells";
-music1.artist = "Sepultura";
-music1.isSingle = false;
-music1.album = "Arise";
-music1.year = 1991;
+Music music1 = new Music();
+
+music1.Name = "Dead Embryonic Cells";
+music1.Artist = "Sepultura";
+music1.IsSingle = false;
+music1.Album = "Arise";
+music1.Year = 1991;
 
 
 Music music2 = new Music();
 
-music2.name = "Escravo da TV";
-music2.artist = "Ratos de Porão";
-music2.isSingle = true;
-music2.year = 1989;
+music2.Name = "Escravo da TV";
+music2.Artist = "Ratos de Porão";
+music2.IsSingle = true;
+music2.Year = 1989;
 
 music1.DisplayMusicInfo();
 music2.DisplayMusicInfo();
+Console.WriteLine(music1.Description);
+Console.WriteLine(music2.Description);
+
